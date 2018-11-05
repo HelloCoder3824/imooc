7c19aa10fcd137b3344c57645c17a974944b6653
jindong <2033802685@qq.com>
2018-11-05 14:24:01 +0800

    第一次更新SelectBrowsers.java

--- src/test/java/com/jd/testutil/SelectBrowsers.java
+++ src/test/java/com/jd/testutil/SelectBrowsers.java
@@ -1,5 +1,8 @@
 package com.jd.testutil;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 /**
  * @class: SelectBrowsers
  * @description:
@@ -7,8 +10,30 @@ package com.jd.testutil;
  * @create: 2018-10-15 15:56
  **/
 public class SelectBrowsers {
-    public static  void  SelectBrowsers(String browsername){
-
+    public static  void  SelectBrowsers(String browsername, WebDriver driver,String url){
+        switch (browsername) {
+            case "chrome":{
+                System.setProperty("webdriver.chrome.driver","E:\\driver\\chromedriver.exe");
+                driver=new ChromeDriver();
+                driver.get(url);
+                driver.manage().window().maximize();
+                break;
+            }
+            case "firefox":{
+                System.setProperty("webdriver.chrome.driver","E:\\driver\\chromedriver.exe");
+                driver=new ChromeDriver();
+                driver.get(url);
+                driver.manage().window().maximize();
+                break;
+            }
+            case "ie":{
+                System.setProperty("webdriver.chrome.driver","E:\\driver\\chromedriver.exe");
+                driver=new ChromeDriver();
+                driver.get(url);
+                driver.manage().window().maximize();
+                break;
+            }
+        }
 
     }
 
